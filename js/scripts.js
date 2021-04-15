aed4aa0ed013ba518c01e05d4c360f394d03dbe5
patrickwilliamweber <patrickwilliamweber@gmail.com>
2021-04-15 17:42:31 -0400

    Just undoing the last bit
    Don't actually want to document.write the pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,2 @@
 let pokemonList = [];
 pokemonList = [{ name: "bulbasaur", height: 7, types: ["grass", "poison"] }, { name: "oddish", height: 5, types: ["acid", "moonlight"] }, { name: "charmander", height: 6, types: ["smokescreen", "metal"] }];
-document.write(pokemonList);
