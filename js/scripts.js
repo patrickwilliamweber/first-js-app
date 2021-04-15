d78cb7e69354809a72e12be4591a9bbe221a15bd
patrickwilliamweber <patrickwilliamweber@gmail.com>
2021-04-15 17:40:57 -0400

    document.write pokemonList
    Just doing this so I can see if it's working

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,3 @@
 let pokemonList = [];
 pokemonList = [{ name: "bulbasaur", height: 7, types: ["grass", "poison"] }, { name: "oddish", height: 5, types: ["acid", "moonlight"] }, { name: "charmander", height: 6, types: ["smokescreen", "metal"] }];
+document.write(pokemonList);
