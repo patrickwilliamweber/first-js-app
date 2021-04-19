b7a06b1ba132b55b76c2a12bb26e393680fe644a
patrickwilliamweber <patrickwilliamweber@gmail.com>
2021-04-19 11:05:21 -0400

    js loop
    js loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
 let pokemonList = [];
 pokemonList = [{ name: "bulbasaur", height: 7, types: ["grass", "poison"] }, { name: "oddish", height: 5, types: ["acid", "moonlight"] }, { name: "charmander", height: 6, types: ["smokescreen", "metal"] }];
+
+for (i = 0; i < pokemonList.length; i++) {
+    console.log `Pokemon number ${i + 1} is ${pokemonList[i].name}`
+}
