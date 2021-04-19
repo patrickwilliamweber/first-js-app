a752e8a1994717886dd2870dad6470108010916b
patrickwilliamweber <patrickwilliamweber@gmail.com>
2021-04-19 11:28:20 -0400

    updating the page
    Added a touch of css for fonts and colors. That's about it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 let pokemonList = [];
-pokemonList = [{ name: "bulbasaur", height: 7, types: ["grass", "poison"] }, { name: "oddish", height: 5, types: ["acid", "moonlight"] }, { name: "charmander", height: 6, types: ["smokescreen", "metal"] }];
+pokemonList = [{ name: "Bulbasaur", height: 7, types: ["grass", "poison"] }, { name: "Oddish", height: 5, types: ["acid", "moonlight"] }, { name: "Charmander", height: 6, types: ["smokescreen", "metal"] }];
 
 for (i = 0; i < pokemonList.length; i++) {
-    document.write(`Pokemon number ${i + 1} is ${pokemonList[i].name}`)
-}
+    document.write(`<p class="list"> Pokemon number ${i + 1} is <strong>${pokemonList[i].name}</strong>, with a height of <strong>${pokemonList[i].height}</strong>.`);
+    if (pokemonList[i].height > 6) {
+        document.write(" (Whoooaaa this guy's massive!) </p> ")
+    }
+};
