702c50d0f58683dbb5fd6b26826594ac4aad6022
patrickwilliamweber <patrickwilliamweber@gmail.com>
2021-04-29 10:11:41 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ let pokemonRepository = (function() {
             list.classList.add('list')
             let listItem = document.createElement('li');
             let button = document.createElement('button');
-            button.innerText = element.name;
+            button.innerText = pokemon.name;
             button.classList.add('button');
             listItem.appendChild(button);
             list.appendChild(listItem);
@@ -36,17 +36,18 @@ let pokemonRepository = (function() {
 
 
 pokemonRepository.getAll().forEach(function(element) {
-    let list = document.querySelector('.pokemon-list');
-    list.classList.add('list')
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = element.name;
-    button.classList.add('button');
-    listItem.appendChild(button);
-    list.appendChild(listItem);
-
-
-
-
-
-})
+        let list = document.querySelector('.pokemon-list');
+        list.classList.add('list')
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = element.name;
+        button.classList.add('button');
+        listItem.appendChild(button);
+        list.appendChild(listItem);
+    })
+    //Below is a test with a pokemon I made up.
+    /*let newPokemon = {
+        name: "Faloo (fuh-LOO)",
+        height: 12
+    }
+    pokemonRepository.addListItem(newPokemon);*/
