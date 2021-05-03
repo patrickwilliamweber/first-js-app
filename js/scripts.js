7d999b8760914fbe435f3e2a95c9939107a04fc7
patrickwilliamweber <patrickwilliamweber@gmail.com>
2021-05-03 09:21:35 -0400

    API fetch
    fetching from the API

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonRepository = (function() {
     let pokemonList = [];
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     pokemonList = [{ name: "Bulbasaur", height: 7, types: ["grass", "poison"] }, { name: "Oddish", height: 5, types: ["acid", "moonlight"] }, { name: "Charmander", height: 6, types: ["smokescreen", "metal"] }];
     return {
         add: function(pokemon) {
@@ -23,10 +24,37 @@ let pokemonRepository = (function() {
             listItem.appendChild(button);
             list.appendChild(listItem);
             button.addEventListener('click', pokemonRepository.showDetails);
-
+        },
+        loadList: function loadList() {
+            return fetch(apiUrl).then(function(response) {
+                return response.json();
+            }).then(function(json) {
+                json.results.forEach(function(item) {
+                    let pokemon = {
+                        name: item.name,
+                        detailsUrl: item.url
+                    };
+                    add(pokemon);
+                });
+            }).catch(function(e) {
+                console.error(e);
+            })
+        },
+        loadDetails: function loadDetails(item) {
+            let url = item.detailsUrl;
+            return fetch(url).then(function(response) {
+                return response.json();
+            }).then(function(details) {
+                // Now we add the details to the item
+                item.imageUrl = details.sprites.front_default;
+                item.height = details.height;
+                item.types = details.types;
+            }).catch(function(e) {
+                console.error(e);
+            });
         },
 
-    };
+    }
 })();
 
 
@@ -43,13 +71,17 @@ let pokemonRepository = (function() {
 //The next line is a test.
 //pokemonRepository.add({ name: "King-Pokemon", height: 12 });
 
+pokemonRepository.loadList().then(function() {
+            // Now the data is loaded!
+            pokemonRepository.getAll().forEach(function(pokemon) {
+                    pokemonRepository.addListItem(pokemon);
 
-pokemonRepository.getAll().forEach(function(element) {
-        pokemonRepository.addListItem(element);
-    })
-    //Below is a test with a pokemon I made up.
-    // let newPokemon = {
-    //     name: "Faloo (fuh-LOO)",
-    //     height: 12
-    // }
-    // pokemonRepository.addListItem(newPokemon);
+                    // pokemonRepository.getAll().forEach(function(element) {
+                    //         pokemonRepository.addListItem(element);
+                })
+                //Below is a test with a pokemon I made up.
+                // let newPokemon = {
+                //     name: "Faloo (fuh-LOO)",
+                //     height: 12
+                // }
+                // pokemonRepository.addListItem(newPokemon);
