d517e63bdbf97fdbb9580a59d7a6cf5b64a59f4b
patrickwilliamweber <patrickwilliamweber@gmail.com>
2021-05-17 10:00:19 -0400

    z index
    zindex

--- js/scripts.js
+++ js/scripts.js
@@ -26,10 +26,8 @@ let pokemonRepository = (function() {
             console.log(event.target.innerText);
         },
         addListItem: function(pokemon) {
-            //fetching the ul from html//
+            //getting the ul from html//
             let list = document.querySelector('.pokemon-list');
-            //adding the class for css purposes//
-            list.classList.add('list');
             //adding list items as 'listItem', they are parents to the buttons, but do not themselves display//
             let listItem = document.createElement('li');
             let button = document.createElement('button');
@@ -38,6 +36,10 @@ let pokemonRepository = (function() {
             //appending listItem to list and button to listItem as children//
             listItem.appendChild(button);
             list.appendChild(listItem);
+            //creating pokemonName as a div, adding hidden and styling classes and the innerText//
+            let pokemonName = document.createElement('p');
+            pokemonName.classList.add('modal-feature');
+            pokemonName.innerText = `${pokemon.name}`;
             //creating height as a div and adding classes for hidden and styling//
             let height = document.createElement('div');
             height.classList.add('hidden');
@@ -46,22 +48,22 @@ let pokemonRepository = (function() {
             let image = document.createElement('div');
             image.classList.add('hidden');
             image.classList.add('image');
-
-
+            //creating the modalDiv and appending image, name, height, and hidden and styling classes//
             let modalDiv = document.createElement('div');
+            //appending modalDiv to button as a sibling//
             button.after(modalDiv);
-            modalDiv.classList.add('modalDiv');
-            modalDiv.classList.add('hidden');
             modalDiv.appendChild(image);
-            let pokemonName = document.createElement('p');
-            pokemonName.classList.add('modal-feature');
-            pokemonName.innerText = `${pokemon.name}`;
             modalDiv.appendChild(pokemonName);
             modalDiv.appendChild(height);
-            button.after(modalDiv);
+            modalDiv.classList.add('modalDiv');
+            modalDiv.classList.add('hidden');
+            //getting the overlay from HTML//
             let overlay = document.getElementById('overlay');
+            //calling pokemonRepository.showDetails on click,  which console logs the pokemon's name//
             button.addEventListener('click', pokemonRepository.showDetails);
+            //defining/calling the function that toggles the visibility of the modal//
             button.addEventListener('click', function() {
+                //loadDetails here is bringing in all the necessary information to fill the html elements//
                 pokemonRepository.loadDetails(pokemon).then(function() {
                     modalDiv.classList.toggle('hidden');
                     height.innerText = `Height: ${pokemon.height}`;
@@ -69,8 +71,8 @@ let pokemonRepository = (function() {
                     image.innerHTML = `<div><img src="${pokemon.imageUrl}" alt="pokemon image"></div>`;
                     image.classList.toggle('hidden');
                     overlay.classList.toggle('active');
-
                 });
+
             });
         },
         loadList: function loadList() {
